f6a32bdb058c90923b7a546ecb0a8964ac276c8d
david4473 <omotayodavid9@gmail.com>
2023-07-04 02:09:09 -0700

    removed mansory grid

--- src/components/explore_section.jsx
+++ src/components/explore_section.jsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
+
 const Explore_section = () => {
   return (
     <section>
-      <div className="w-full h-full">
-        <div className="gap-3 columns-2 md:columns-3 lg:columns-3">
-          <img className="" src="/images/image-19.png" alt="" />
-          <img className="" src="/images/image-20.png" alt="" />
-          <img className="" src="/images/image-21.png" alt="" />
-          <img className="" src="/images/image-22.png" alt="" />
-          <img className="" src="/images/image-23.png" alt="" />
+      <div className="w-full h-full relative">
+        <div className="w-full h-full inset-0">
+          <Image
+            className="object-contain"
+            src={"/masonry.png"}
+            fill
+            priority
+          />
         </div>
       </div>
     </section>
