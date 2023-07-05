5f56c90f7f9417828bd2726d9dfc2560f164009b
david4473 <omotayodavid9@@gmail.com>
2023-07-05 05:07:22 -0700

    finished expore section

--- src/components/explore_section.jsx
+++ src/components/explore_section.jsx
@@ -4,13 +4,17 @@ const Explore_section = () => {
   return (
     <section>
       <div className="w-full h-full relative">
-        <div className="w-full h-full inset-0">
+        <div className="w-full md:py-52 py-36 -z-10">
           <Image
-            className="object-contain"
+            className="md:object-cover object-contain brightness-50 "
             src={"/masonry.png"}
             fill
             priority
           />
+        </div>
+        <div className="text-center h-full w-full absolute top-0 left-0 flex justify-center items-center flex-col gap-4">
+          <h3 className="text-5xl font-bold text-slate-50">Explore Gallery</h3>
+          <img src="/images/chev.svg" alt="" />
         </div>
       </div>
     </section>
